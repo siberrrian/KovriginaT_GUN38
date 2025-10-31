02589d963d44a35f537213cf7ebfcf6cf0162637
siberrrian <siberrrian@gmail.com>
2025-10-31 16:17:58 +0300

    Import CoroutineHomeworkStart.unitypackage

---
+++ Assets/Homework/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Netologia.Homework
+{
+	public class Player : MonoBehaviour
+	{
+		private bool _ready;
+		private Rigidbody _ball;
+		
+		[SerializeField]
+		private Rigidbody _ballPrefab;
+		[SerializeField]
+		private float _startVelocity;
+		[SerializeField]
+		private float _lifetime;
+
+		[SerializeField]
+		private float _respawnDelay;
+
+		private void Update()
+		{
+			if (!_ready) return;
+			if (Input.GetKey(KeyCode.Space))
+			{
+				StartCoroutine(Reloader());
+				_ball.isKinematic = false;
+				_ball.transform.parent = null;
+				_ball.velocity = transform.forward * _startVelocity;
+				Destroy(_ball.gameObject, _lifetime);
+			}
+		}
+
+		private IEnumerator Reloader()
+		{
+			_ready = false;
+			yield return new WaitForSeconds(_respawnDelay);
+			Spawn();
+		}
+
+		private void Spawn()
+		{
+			_ball = Instantiate(_ballPrefab, transform);
+			_ball.isKinematic = true;
+			_ready = true;
+		}
+
+		private void Start()
+		{
+			Spawn();
+		}
+	}
+}
