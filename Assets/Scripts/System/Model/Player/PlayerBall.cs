5db9c5e9cbced463da2bd3d889d41dd52e9d1f3b
siberrrian <siberrrian@gmail.com>
2026-05-08 23:32:52 +0300

    - location completed - camera movement added

--- Assets/Scripts/System/Model/Player/PlayerBall.cs
+++ Assets/Scripts/System/Model/Player/PlayerBall.cs
@@ -11,16 +11,16 @@ namespace System.Model
     public sealed class PlayerBall : PlayerBase
     {
         [SerializeField] private Rigidbody _rigidbody;
-        [SerializeField] private int _speed;
         [SerializeField] private int _health = 100;
+        [SerializeField] private Camera _camera;
         [SerializeField] private TextMeshProUGUI _textHealth;
 
         [Inject] private IAxisInput _input;
         private IDisposable _disposable;
+        /*
+        private void OnEnable() => _disposable = _input.IAxisInput.Subscribe(Move);
 
-        //private void OnEnable() => _disposable = _input.IAxisInput.Subscribe(Move);
-
-        //private void OnDisable() => _disposable.Dispose();
+        private void OnDisable() => _disposable.Dispose();*/
 
         protected override void Move(Vector3 direction) => _rigidbody.AddForce(direction * Speed);
 
@@ -29,11 +29,18 @@ namespace System.Model
             _textHealth.text = _health.ToString();
         }
         void Update()
-        {
+        {/*
             float moveH = Input.GetAxis("Horizontal");
             float moveV = Input.GetAxis("Vertical");
-            _rigidbody.AddForce(new Vector3(moveH, 0, moveV) * _speed);
-           // _textHealth.text = _health.ToString();
+            _rigidbody.AddForce(new Vector3(moveH, 0, moveV) * Speed);*/
+
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            _rigidbody.velocity = new Vector3(move.x * Speed, _rigidbody.velocity.y, move.z * Speed);
         }
 
         public void ChangeHealth(int bonus = 15)
